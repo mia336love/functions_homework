504944ca0bd9cc8b1905fc8064b7dcabd91d82b8
mia336love <122865270+mia336love@users.noreply.github.com>
2023-03-01 22:53:00 +0300

    2 ex done

--- 2_ex.js
+++ 2_ex.js
@@ -1,3 +1,14 @@
 for (let i = 1; i <= 100; i++) {
-  console.log(i);
+  //console.log(i);
+  if (1 <= i && i <= 17) {
+    console.log(i, "ребенок");
+  }
+  if (18 <= i && i <= 30) {
+    console.log(i, "молодой");
+  }
+  if (30 <= i && i <= 55) {
+    console.log(i, "взрослый");
+  } else if (i > 55) {
+    console.log(i, "старый");
+  }
 }
