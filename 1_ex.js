be25a354e747e9cb8f27e056dbc76a12e14f7c4d
mia336love <122865270+mia336love@users.noreply.github.com>
2023-03-02 01:44:13 +0300

    1 ex doesnt work

--- 1_ex.js
+++ 1_ex.js
@@ -1,4 +1,18 @@
 numbers = [1, 6, 5, 87, 13, 16, 22, 336];
 
 let rangeNum = [];
-function isNumberInRange(numbers) 
+function isNumberInRange(numbers) {
+  for (i in numbers) {
+    if (0 < i && i < 10) {
+      rangeNum.push(numbers[i]);
+    }
+  }
+
+  return rangeNum;
+  /*
+  if (0 < numbers && numbers < 10) {
+    return true;
+  }*/
+}
+let result = isNumberInRange(numbers);
+console.log(result);
--- 4_ex.js
+++ 4_ex.js
@@ -2,7 +2,14 @@ const array = [20, 16, 17, 32, 16, 17, 20, 33, 15];
 
 uniqArray = [];
 function uniqueValues(array) {
-  uniqArray = new set(array);
+  for (let i = 0; i < array.length; i++) {
+    if (uniqArray.includes(array[i] === false)) {
+      uniqArray.push(array[i]);
+    }
+  }
+  return uniqArray;
 }
-const result = uniqueValues(array);
-console.log(result);
+
+//const result = uniqueValues(array);
+//console.log(result);
+console.log(uniqueValues(array));
