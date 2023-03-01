212469dae585494e029b9670b63357f85dd6829b
mia336love <122865270+mia336love@users.noreply.github.com>
2023-03-02 01:02:37 +0300

    1 ex delete rave

--- 1_ex.js
+++ 1_ex.js
@@ -1,13 +1,4 @@
 numbers = [1, 6, 5, 87, 13, 16, 22, 336];
 
 let rangeNum = [];
-function isNumberInRange(numbers) {
-  for (let num in numbers) {
-    if (0 < numbers.includes([num]) < 10) {
-      rangeNum.push(numbers[num]);
-    }
-  }
-  return rangeNum;
-}
-let result = isNumberInRange(numbers);
-console.log(result);
+function isNumberInRange(numbers) 
